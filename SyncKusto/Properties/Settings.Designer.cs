c8a95e8b843bd416ca18ed7681d67e8af84d658b
Andy Ladwig <anladwig@microsoft.com>
2022-06-11 22:33:44 -0500

    Make .csl the default file extension option

--- SyncKusto/Properties/Settings.Designer.cs
+++ SyncKusto/Properties/Settings.Designer.cs
@@ -109,7 +109,7 @@ namespace SyncKusto.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("False")]
+        [global::System.Configuration.DefaultSettingValueAttribute("True")]
         public bool UseLegacyCslExtension {
             get {
                 return ((bool)(this["UseLegacyCslExtension"]));
