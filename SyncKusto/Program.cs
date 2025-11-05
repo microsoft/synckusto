c76663a59a9da9534b229d5fbb8508651f4d91a3
Ben Martens <benmar@microsoft.com>
2025-11-05 10:57:56 -0800

    Revert high dpi defaults

--- SyncKusto/Program.cs
+++ SyncKusto/Program.cs
@@ -1,10 +1,9 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-using SyncKusto.Properties;
 using System;
-using System.Diagnostics;
 using System.Windows.Forms;
+using System.Drawing;
 
 namespace SyncKusto
 {
@@ -13,6 +12,10 @@ namespace SyncKusto
         [STAThread]
         static void Main(string[] args)
         {
+            // Revert some changes that came from the upgrade to .NET 8 regarding high DPI settings
+            Application.SetHighDpiMode(HighDpiMode.DpiUnaware);
+            Application.SetDefaultFont(new Font("Microsoft Sans Serif", 8.25f));
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new MainForm());
