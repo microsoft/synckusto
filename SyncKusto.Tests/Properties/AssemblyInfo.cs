47afaad959275ef138196594e2a167c6b49d1875
Ben Martens <benmar@microsoft.com>
2025-11-05 10:18:36 -0800

    tool output

--- SyncKusto.Tests/Properties/AssemblyInfo.cs
+++ SyncKusto.Tests/Properties/AssemblyInfo.cs
@@ -1,21 +1,10 @@
 using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("SyncKusto.Tests")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("SyncKusto.Tests")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 [assembly: ComVisible(false)]
 
 [assembly: Guid("52462172-b8ca-45ce-a2c6-a189cc786f64")]
-
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
   
